20da1cb95f4384d37ef586decc8c667340428bf9
Giovanni Karra <karragiovanni@gmail.com>
2024-03-13 12:19:29 +0100

    essaie qui ne fonctionne pas encore mais ça arrive (????)

--- LinearElasticity/src/homework.c
+++ LinearElasticity/src/homework.c
@@ -40,6 +40,16 @@ void geoMeshGenerate() {
 }
 
 
+void femElasticLocal(femMesh *theMesh, const int iElem, int *map, double *x, double *y)
+{
+    for (int j = 0; j < theMesh->nLocalNode; j++){
+        map[j] = theMesh->elem[theMesh->nLocalNode*iElem + j];
+        x[j] = theMesh->nodes->X[map[j]];
+        y[j] = theMesh->nodes->Y[map[j]];
+    }
+}
+
+
 double *femElasticitySolve(femProblem *theProblem)
 {
 
@@ -52,7 +62,7 @@ double *femElasticitySolve(femProblem *theProblem)
     
     
     double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
-    int iElem,iInteg,iEdge,i,j,d,map[4],mapX[4],mapY[4];
+    int iElem,iInteg,iEdge,j,d,map[4],mapX[4],mapY[4], i;
     
     int nLocal = theMesh->nLocalNode;
 
@@ -64,10 +74,46 @@ double *femElasticitySolve(femProblem *theProblem)
     double **A = theSystem->A;
     double *B  = theSystem->B;
     
-    
-  //
-  //  A faire :-)
-  //                
+    for (iElem = 0; iElem < theMesh->nElem; iElem++)
+    {
+        femElasticLocal(theMesh, iElem, map, x, y);
+        for (iInteg=0; iInteg < theRule->n; iInteg++){  
+            double xsi    = theRule->xsi[iInteg];
+            double eta    = theRule->eta[iInteg];
+            double weight = theRule->weight[iInteg];  
+            femDiscretePhi2(theSpace,xsi,eta,phi);
+            femDiscreteDphi2(theSpace,xsi,eta,dphidxsi,dphideta);
+
+            double dxdxsi = 0;
+            double dxdeta = 0;
+            double dydxsi = 0; 
+            double dydeta = 0;
+            
+            for (i = 0; i < theSpace->n; i++){  
+                dxdxsi += x[i]*dphidxsi[i];       
+                dxdeta += x[i]*dphideta[i];   
+                dydxsi += y[i]*dphidxsi[i];   
+                dydeta += y[i]*dphideta[i]; 
+            }
+
+
+            double jac = dxdxsi * dydeta - dxdeta * dydxsi;
+
+            for (i = 0; i < theSpace->n; i++){    
+                dphidx[i] = (dphidxsi[i] * dydeta - dphideta[i] * dydxsi) / jac;       
+                dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / jac; 
+            }  
+
+            for (i = 0; i < theSpace->n; i++){ 
+                for(j = 0; j < theSpace->n; j++){
+                    theSystem->A[map[i]][map[j]] += (dphidx[i] * dphidx[j] + dphidy[i] * dphidy[j]) * jac * weight; 
+                }
+            }                                                                                            
+            for (i = 0; i < theSpace->n; i++){
+                theSystem->B[map[i]] += phi[i] * jac *weight; 
+            }
+        }
+    }
                 
                 
   
