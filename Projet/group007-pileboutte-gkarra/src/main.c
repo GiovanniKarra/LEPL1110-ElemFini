b07d689d2083ac59cf66d5df2c640df9e9af471c
Giovanni Karra <karragiovanni@gmail.com>
2024-03-15 22:45:28 +0100

    intermédiaire

---
+++ Projet/group007-pileboutte-gkarra/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "gmshc.h"
+
+int main(int argc, char const *argv[]) {
+    printf("test hello");
+    return 0;
+}
